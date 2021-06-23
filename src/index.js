0717e163f215fadc8866d336b53de2f45d684349
Jongmin Lee <jongminlee8910@gmail.com>
2021-06-23 19:16:59 +0900

    2차 피드백 초기세팅 수정완료

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Routes from "./Routes";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './Routes';
+import './styles/common.scss';
+import './styles/variable.scss';
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(<Routes />, document.getElementById('root'));
