39f206f4535bfef30bfd7ac4c96b5e8759672846
yunkyung <poew91@gmail.com>
2021-06-24 13:57:36 +0900

    [Add] : 팀 세팅 설정 후 파일 경로 재설정 & 화면 띄우기 완료

--- src/pages/yunkyung/Login/Login.js
+++ src/pages/yunkyung/Login/Login.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.scss';
 
-class LoginYunkyung extends React.Component {
+class Login extends React.Component {
   render() {
-    return <div>11</div>;
+    return (
+      <div className="login_bd">
+        <h1>Westagram</h1>
+
+        <div>
+          <input
+            type="text"
+            id="userId"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <br />
+          <input type="password" id="password" placeholder="비밀번호" />
+        </div>
+        <div className="btn">
+          <button type="button" class="button" id="button">
+            로그인
+          </button>
+          <footer>
+            <a>비밀번호를 잊으셨나요?</a>
+          </footer>
+        </div>
+      </div>
+    );
   }
 }
-export default LoginYunkyung;
+
+export default Login;
