a81f28e180f013fe9c016cc2ecbc23ddc8815855
yunkyung <poew91@gmail.com>
2021-06-29 15:01:15 +0900

    [Add] : Mission - Mock data 활용하기 2차 수정

--- src/pages/yunkyung/Main/Comment.js
+++ src/pages/yunkyung/Main/Comment.js
@@ -5,15 +5,18 @@ class Comment extends React.Component {
   render() {
     const { commentList } = this.props;
 
+    // json으로 사용할 때는 {this.state.CommentData.map(comment)} 로 사용한다.
     return (
       <>
         <ul className="comment_list">
-          {commentList.map((comment, i) => (
-            <li key={i} className="comment_box">
-              <span className="comment_id">{comment.userName}</span>
-              {comment.content}
-            </li>
-          ))}
+          {commentList.map(comment => {
+            return (
+              <li key={comment.id} className="comment_box">
+                <span className="comment_id">{comment.userName}</span>
+                {comment.content}
+              </li>
+            );
+          })}
         </ul>
       </>
     );
